52937c0ff99091fe4de126033a91177cefb340fb
katia-kaci <katia.kaci.1@ens.etsmtl.ca>
2025-06-24 00:25:55 -0400

    Sauvegarder si l'app est fermée

--- context/ModeContext.js
+++ context/ModeContext.js
@@ -1,11 +1,26 @@
 import React, { createContext, useState, useContext } from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const ModeContext = createContext();
 
 export const ModeProvider = ({ children }) => {
   const [mode, setMode] = useState('solo'); // par défaut solo
+
+  useEffect(() => {
+    const loadMode = async () => {
+      const storedMode = await AsyncStorage.getItem('mode');
+      if (storedMode) setMode(storedMode);
+    };
+    loadMode();
+  }, []);
+
+  const updateMode = async (newMode) => {
+    setMode(newMode);
+    await AsyncStorage.setItem('mode', newMode);
+  };
+
   return (
-    <ModeContext.Provider value={{ mode, setMode }}>
+    <ModeContext.Provider value={{ mode, setMode: updateMode }}>
       {children}
     </ModeContext.Provider>
   );
